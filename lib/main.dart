e7adfbb3f9c3369832e4e4c4aa992eadaa305314
薛强强 <xueqq@elinkint.com>
2019-12-30 13:55:38 +0800

    计数器实现

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Demo1"),
+          title: Text("Demo2"),
         ),
       body: HomeContent(),
       )
@@ -17,51 +17,57 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomeContent extends StatelessWidget {
+class HomeContent extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Padding(
-      padding: EdgeInsets.all(10),
-      child: ListView(
-        children: <Widget>[
-          ProductItem("Apple1","iPhone Product1","https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577444657653&di=54faa0b266faf0e00cc020c54dec8f8e&imgtype=0&src=http%3A%2F%2Fpic1.zhimg.com%2Fv2-b86f1b91c70787850146f07db1bf97a4_b.jpg"),
-          ProductItem("Apple2","iPhone Product2","https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577444657653&di=54faa0b266faf0e00cc020c54dec8f8e&imgtype=0&src=http%3A%2F%2Fpic1.zhimg.com%2Fv2-b86f1b91c70787850146f07db1bf97a4_b.jpg"),
-          ProductItem("Apple3","iPhone Product3","https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1577444657653&di=54faa0b266faf0e00cc020c54dec8f8e&imgtype=0&src=http%3A%2F%2Fpic1.zhimg.com%2Fv2-b86f1b91c70787850146f07db1bf97a4_b.jpg")
-        ],
-      ),
-    );
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return HomeContentState();
   }
 }
 
-class ProductItem extends StatelessWidget {
-  final String title;
-  final String subTitle;
-  final String imageUrl;
+class HomeContentState extends State<HomeContent> {
+  int count = 0;
 
-  ProductItem(this.title,this.subTitle,this.imageUrl);
+  @override  //初始化操作：可以在内做网络请求 初始化内容等等 类似viewdidload
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Center(
-        child: Container(
-          padding: EdgeInsets.all(5),
-          decoration: BoxDecoration(
-            border: Border.all(width: 2)
-          ),
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 10,),
-              Text(title,style: TextStyle(fontSize: 25),),
-              Text(subTitle,style: TextStyle(fontSize: 18),),
-              SizedBox(height: 10,),
-              Image.network(imageUrl)
-            ],
-          ),
-        ),
-    );
-
+        child:Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("当前计数为：$count",style: TextStyle(fontSize: 26),),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                  onPressed: () {
+                    setState(() {
+                      count ++;
+                    });
+                  },
+                  child: Text("+1"),
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    setState(() {
+                      count --;
+                    });
+                  },
+                  child: Text("-1"),
+                )
+              ],
+            )
+          ],
+        )
+    );;
   }
 }
+
+
 
