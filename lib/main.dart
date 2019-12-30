26a1232f5b10525b4ae1881dfb41a89fec85a785
薛强强 <xueqq@elinkint.com>
2019-12-30 17:49:11 +0800

    save

--- lib/main.dart
+++ lib/main.dart
@@ -18,49 +18,99 @@ class MyApp extends StatelessWidget {
 }
 
 class HomeContent extends StatefulWidget {
-  HomeContent() {
-    print("HomeContent构造函数");
-  }
-
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
-    print("HomeContent creatState");
     return HomeContentState();
   }
 }
 
 class HomeContentState extends State<HomeContent> {
-
-  HomeContentState() {
-    print("HomeContentState 构造函数");
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    print("HomeContentState didChangeDependencies");
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    print("HomeContentState initState");
-  }
-
   @override
   Widget build(BuildContext context) {
-    print("HomeContentState build");
-    return Center(
-      child: Text("Hello World",style: TextStyle(fontSize: 30),),
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Column(
+        children: <Widget>[
+          SizedBox(height: 20),
+          Center(
+            child: avatarImageView() //头像
+          ),
+          SizedBox(height: 50),
+          AccountPWDFormWidget(),
+        ],
+      ),
     );
-  }
-
-  @override
-  void didUpdateWidget(HomeContent oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    print("HomeContentState didUpdateWidget");
   }
 }
 
+//头像
+class avatarImageView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return CircleAvatar(
+        radius: 40,
+        backgroundImage: NetworkImage("https://teacherxue.github.io/images/default_avatar.jpg")
+    );
+  }
+}
 
+//帐号密码表单
+class AccountPWDFormWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return AccountPWDFormState();
+  }
+}
+
+class AccountPWDFormState extends State<AccountPWDFormWidget> {
+
+//  final registerFormKey = GlobalKey<FormState>();
+//  String username, password;
+//
+//  AccountPWDFormState() {
+//    registerFormKey.currentState.save();
+//  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          TextFormField(
+            decoration: InputDecoration(
+                icon: Icon(Icons.people),
+                labelText: "用户名或手机号"
+            ),
+            onSaved: (value) {
+              print("用户名：$value");
+            },
+          ),
+          TextFormField(
+            obscureText: true,
+            decoration: InputDecoration(
+                icon: Icon(Icons.lock),
+                labelText: "密码"
+            ),
+            onSaved: (value) {
+              print("密码：$value");
+            },
+          ),
+          SizedBox(height: 16,),
+          Container(
+            width: double.infinity,
+            height: 44,
+            child: RaisedButton(
+              color: Colors.lightGreen,
+              child: Text("注 册", style: TextStyle(fontSize: 20, color: Colors.white),),
+              onPressed: () {
+                print("点击了注册按钮");
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
